03f562f493e78ab33d5e397cf7892ea9d563d495
Nirina Rakotoniary <nirinamercia@mail.com>
2023-12-20 18:52:15 +0100

    done

---
+++ code_1.py
@@ -0,0 +1,26 @@
+def share_candy(enfants, bonbons) :
+    res = {}
+    for e in enfants :
+        res[e] = {}
+        for v in enfants[e]:
+            if(bonbons[v] > 0):
+                res[e][v] = 1
+                bonbons[v] -=  1
+    for x in res :
+        print(x,":",res[x])
+    print(bonbons)
+    return res
+
+enfants = {
+    "Alice" : ["chocolat", "guimauve"],
+    "Bob" : ["caramel", "fruit"],
+    "Charlie" : ["chocolat", "caramel"]
+    }
+bonbons = {
+    "chocolat" : 10,
+    "caramel" : 8,
+    "guimauve" : 5,
+    "fruit" : 6
+}
+
+share_candy(enfants, bonbons)
